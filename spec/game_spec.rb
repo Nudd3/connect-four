fe9eff7ecb89c60bea0b79f488376da79e207bd2
Noak Pettersson <noake12@gmail.com>
2022-04-22 04:17:46 +0200

    Skeleton written for testing #player_turn and #verify_input

--- spec/game_spec.rb
+++ spec/game_spec.rb
@@ -11,5 +11,29 @@
 # What methods needs to be checked?
 # - play -> No, since all methods are tested elsewhere
 # - player_turn -> Yes, make sure that it's every other
-# - player_input -> Yes, make sure that input is good
+# - player_input -> Yes, make sure that input is verified
 # - game_over? -> Yes, if there's a winner or a tie
+
+describe Game do
+  describe '#player_turn' do
+    context 'after each play the turn swaps' do
+      it 'is player1 before first play'
+
+      it 'is player2 after first play'
+    end
+  end
+
+  describe '#verify_input' do
+    context 'when input is good' do
+      it 'is truthy'
+    end
+
+    context 'when input is bad' do
+      it 'is falsy when input a digit outside 1-7'
+
+      it 'is falsy when input is not a digit'
+
+      it 'is falsy when column if full'
+    end
+  end
+end
